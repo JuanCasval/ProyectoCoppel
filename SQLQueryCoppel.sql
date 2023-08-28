4a4295d3f87138b018a14f1cc22cab7e9641afe9
JuanCasval <juanp.castrov@hotmail.com>
2023-08-28 10:16:20 -0700

    Se añade documento solicitado y scripts de base de datos

---
+++ SQLQueryCoppel.sql
@@ -0,0 +1,71 @@
+﻿
+/*Creacion del catalago de Roles */
+CREATE TABLE Roles( 
+ IdRole int IDENTITY(1,1) not null,
+ Descripcion varchar(50) not null,
+ Bono float not null,
+ PRIMARY KEY (IdRole)
+
+);
+
+/*Insercion de datos base del catalago de Roles */
+INSERT INTO Roles (Descripcion,Bono) values ('Chofer',10);
+INSERT INTO Roles (Descripcion,Bono) values ('Cargador',5);
+INSERT INTO Roles (Descripcion,Bono) values ('Auxiliar',0);
+
+/*Creacion del catalago de Meses */
+
+CREATE TABLE Months( 
+ IdMes int IDENTITY(1,1) not null,
+ Descripcion varchar(50) not null,
+ PRIMARY KEY (IdMes)
+
+);
+
+/*Insercion de datos base del catalago de Meses */
+INSERT INTO Months (Descripcion) values ('Enero');
+INSERT INTO Months (Descripcion) values ('Febrero');
+INSERT INTO Months (Descripcion) values ('Marzo');
+INSERT INTO Months (Descripcion) values ('Abril');
+INSERT INTO Months (Descripcion) values ('Mayo');
+INSERT INTO Months (Descripcion) values ('Junio');
+INSERT INTO Months (Descripcion) values ('Julio');
+INSERT INTO Months (Descripcion) values ('Agosto');
+INSERT INTO Months (Descripcion) values ('Septiembre');
+INSERT INTO Months (Descripcion) values ('Octubre');
+INSERT INTO Months (Descripcion) values ('Noviembre');
+INSERT INTO Months (Descripcion) values ('Diciembre');
+
+/*Creacion del catalago de Empleados */
+
+CREATE TABLE Employees( 
+ Id int IDENTITY(1,1) not null,
+ IdEmployee int UNIQUE not null ,
+ Nombre varchar(50) not null,
+ IdRole int not null,
+ SueldoBase float not null,
+ estado char not null,
+ PRIMARY KEY (Id),
+ CONSTRAINT FK_EmployeeRole FOREIGN KEY (IdRole) REFERENCES Roles(IdRole)
+
+);
+
+/*INSERT INTO Employees (IdEmployee,Nombre,IdRole,SueldoBase) values (1,'Juan Pablo Castro Valdez',1,30);*/
+
+/*Creacion del catalogo de movimientos*/
+
+CREATE TABLE Movimientos( 
+ IdMovimiento int IDENTITY(1,1) not null,
+ Id int not null,
+ IdEmployee int not null,
+ Nombre varchar(50),
+ IdRole int not null,
+ Cantidad_entregas int not null,
+ IdMes int not null,
+ periodo int not null DEFAULT(YEAR(GETDATE)),
+ PRIMARY KEY (IdMovimiento),
+ CONSTRAINT FK_MovEmployee FOREIGN KEY (Id) REFERENCES Employees(Id),
+ CONSTRAINT FK_MovRole FOREIGN KEY (IdRole) REFERENCES Roles(IdRole),
+ CONSTRAINT FK_MovMonth FOREIGN KEY (IdMes) REFERENCES Months(IdMes)
+
+);
---
+++ StoredProcedures.sql
@@ -0,0 +1,65 @@
+﻿CREATE PROCEDURE dbo.SaveEmployee @IdEmployee int, @Name varchar(50), @IdRol int 
+AS 
+DECLARE @Sueldo float;
+SET @Sueldo = 30;
+INSERT INTO Employees (IdEmployee,Nombre,IdRole,SueldoBase,estado) values (@IdEmployee,@Name,@IdRol,@Sueldo,'A');
+GO
+/*exec SaveEmployee 4,'Jose Manuel Armenta',1*/
+
+CREATE PROCEDURE dbo.SaveMovimiento @IdEmployee int, @IdRol int, @CantidadEntregas int, @Mes int
+AS
+DECLARE @NameEmpleado varchar(50);
+DECLARE @Id int; 
+
+SET @NameEmpleado = (SELECT Nombre FROM Employees WHERE IdEmployee = @IdEmployee);
+SET @Id = (SELECT Id FROM Employees WHERE IdEmployee = @IdEmployee);
+
+INSERT INTO Movimientos (Id,IdEmployee,Nombre,IdRole,Cantidad_entregas,IdMes) values (@Id,@IdEmployee,@NameEmpleado,@IdRol,@CantidadEntregas,@Mes);
+GO
+
+/*exec SaveMovimiento 4,1,450,9*/
+
+CREATE PROCEDURE dbo.ConsultaSueldos @Mes int
+AS
+SELECT *, (SueldoMensual + PagoEntregas + BonoHora - RetencionIsr + ValesDespensa) AS SueldoTotal  FROM (
+	SELECT *, CASE
+				WHEN ((SueldoMensual + PagoEntregas + BonoHora)) <= 10000 THEN ((SueldoMensual + PagoEntregas + BonoHora)) * 0.09
+				ELSE ((SueldoMensual + PagoEntregas + BonoHora)) * 0.12
+			END AS RetencionIsr,
+			((SueldoMensual + PagoEntregas + BonoHora) * 0.04) AS ValesDespensa
+	FROM (
+			SELECT A.IdEmployee,
+				A.Nombre,
+				192 as HorasTrabajadas,
+				 C.Descripcion as Rol,
+				(B.SueldoBase * 192) AS SueldoMensual, 
+				(A.Cantidad_entregas * 5) AS PagoEntregas, 
+				(C.Bono * 192) AS BonoHora
+		
+			FROM Movimientos A 
+				RIGHT JOIN Employees B 
+					ON B.IdEmployee = A.IdEmployee 
+				RIGHT JOIN Roles C 
+					ON C.IdRole = A.IdRole
+				RIGHT JOIN Months D 
+					ON D.IdMes = A.IdMes 
+			WHERE A.IdMes = @Mes
+			) as totales 
+		) as TOTAL;
+GO
+
+/*exec ConsultaSueldos 8*/
+
+
+CREATE PROCEDURE dbo.BuscarId @IdEmployee int
+AS 
+SELECT IdEmployee as Result FROM Employees where IdEmployee = @IdEmployee;
+GO
+
+CREATE PROCEDURE dbo.BuscarMov @IdEmployee int, @IdMes int
+AS 
+	SELECT IdEmployee, Nombre, IdMes 
+	FROM Movimientos 
+	where IdEmployee = @IdEmployee
+	AND IdMes = @IdMes;
+GO
